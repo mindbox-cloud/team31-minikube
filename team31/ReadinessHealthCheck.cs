ec6c3cb0bfc3d5938173f3e283c9be051705d3bb
Andrey Chibisov <chibisov@mindbox.cloud>
2024-07-30 18:29:41 +0300

    Probes fix

--- team31/ReadinessHealthCheck.cs
+++ team31/ReadinessHealthCheck.cs
@@ -4,9 +4,18 @@ namespace team31;
 
 public class ReadinessHealthCheck : IHealthCheck
 {
+    private static int? _ostatok = null;
     public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = new CancellationToken())
     {
-        return Task.FromResult(
-            HealthCheckResult.Healthy("A healthy result."));
+        if (_ostatok is null)
+        {
+            var random = new Random();
+            var result = random.Next(0, 3);
+            _ostatok = result;
+        }
+        
+        return DateTime.Now.Minute % 3 != _ostatok
+            ? Task.FromResult(HealthCheckResult.Healthy("A healthy result."))
+            : Task.FromResult(HealthCheckResult.Unhealthy("An unhealthy result."));
     }
 }
