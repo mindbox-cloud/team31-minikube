07bcdccd8a647d5fb24bffb40e3257abd27bade6
Ilya Kudryashov <kudryashov@mindbox.cloud>
2024-07-30 18:56:42 +0300

    deleted file

--- docker-env.sh
+++
@@ -1,7 +0,0 @@
-export DOCKER_TLS_VERIFY="1"
-export DOCKER_HOST="tcp://127.0.0.1:50833"
-export DOCKER_CERT_PATH="/Users/gaynutdinova/.minikube/certs"
-export MINIKUBE_ACTIVE_DOCKERD="minikube"
-
-# To point your shell to minikube's docker-daemon, run:
-# eval $(minikube -p minikube docker-env)
