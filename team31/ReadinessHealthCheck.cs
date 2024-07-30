3f0876f428d815db3071d9a8091f97506b8f8c5d
Ilya Kudryashov <kudryashov@mindbox.cloud>
2024-07-30 18:53:47 +0300

    added helmfile

--- team31/ReadinessHealthCheck.cs
+++ team31/ReadinessHealthCheck.cs
@@ -4,17 +4,17 @@ namespace team31;
 
 public class ReadinessHealthCheck : IHealthCheck
 {
-    private static int? _ostatok = null;
+    private static int? _remainder = null;
     public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = new CancellationToken())
     {
-        if (_ostatok is null)
+        if (_remainder is null)
         {
             var random = new Random();
             var result = random.Next(0, 3);
-            _ostatok = result;
+            _remainder = result;
         }
         
-        return DateTime.Now.Minute % 3 != _ostatok
+        return DateTime.Now.Minute % 3 != _remainder
             ? Task.FromResult(HealthCheckResult.Healthy("A healthy result."))
             : Task.FromResult(HealthCheckResult.Unhealthy("An unhealthy result."));
     }
